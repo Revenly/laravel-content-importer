bad38ed778525b3e1c09a085c9c89867c75910ee
lacasera <aboateng62@gmail.com>
2021-04-19 17:23:51 +0000

    Updated ReadMe

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@
  * You can place your custom package configuration in here.
  */
 return [
-    "directory" => env('IMPORT_DIRECTORY', 'imports'),
+    "directory" =>'imports',
 
-    "chunck_size" => env('IMPORT_CHUNCK_SIZE', 1000),
+    "chunck_size" => 1000,
 
     "heading_row" => 1
 ];
