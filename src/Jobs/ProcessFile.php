33cc0ec83d7a1f9f4b9733471a75b2efac1243d9
Agyenim Boateng Barfi <aboateng62@gmail.com>
2021-09-06 19:24:14 +0000

    uncomment logs

--- src/Jobs/ProcessFile.php
+++ src/Jobs/ProcessFile.php
@@ -69,7 +69,7 @@ class ProcessFile implements ShouldQueue
                 ->chunk(100)
                 ->each(function ($chunk) {
                     $records = array_map(fn ($record) => array_change_key_case($record, CASE_LOWER), $chunk->toArray());
-                    logger()->debug(array_values($records));
+                    //logger()->debug(array_values($records));
                     ImportedContent::create([
                         'file_id' => $this->file->id,
                         'data' => array_values($records)
